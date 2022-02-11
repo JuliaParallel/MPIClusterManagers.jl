6ce384ebcc8c0f18b337cce88c47bc415830c167
Simon Byrne <simonbyrne@gmail.com>
2022-02-11 09:37:08 -0800

    error

--- src/mpimanager.jl
+++ src/mpimanager.jl
@@ -144,7 +144,7 @@ function Distributed.launch(mgr::MPIManager, params::Dict,
                 throw(ErrorException("Reuse of MPIManager is not allowed."))
             end
             cookie = Distributed.cluster_cookie()
-            setup_cmds = "using Distributed; import MPIClusterManagers; MPIClusterManagers.setup_worker($(repr(string(mgr.ip))),$(port),$(repr(cookie)); threadlevel=$(repr(params[:threadlevel])))"
+            setup_cmds = "using Distributed; import MPIClusterManagers; MPIClusterManagers.setup_worker($(repr(string(mgr.ip))),$(mgr.port),$(repr(cookie)); threadlevel=$(repr(params[:threadlevel])))"
             MPI.mpiexec() do mpiexec
                 mpiexec = something(params[:mpiexec], mpiexec)
                 mpiflags = params[:mpiflags]
